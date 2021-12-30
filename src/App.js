c7a91f40f5caceb6b2815084480163244fb5f697
Brian Gleason <51419601+BrianGleason@users.noreply.github.com>
2021-12-30 10:07:23 -0500

    filtered list of classes based on quarter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
+/*
 const schedule = {
   "title": "CS Courses for 2018-2019",
   "courses": {
@@ -26,6 +26,7 @@ const schedule = {
     }
   }
 };
+*/
 
 const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
 
@@ -44,29 +45,72 @@ const getCourseTime = course => (
 const Course = ({ course }) => (
   <div className="card m-1 p-2">
     <div className="card-body">
-      <div className="card-title">{ getCourseTerm(course) } { getCourseNumber(course) }</div>
+      <div className="card-title">{ getCourseTerm(course) } { getCourseNumber(course) } { getCourseTime(course) }</div>
       <div className="card-text">{ course.title }</div>
-      <div className="card-time">{ getCourseTime(course) }</div>
-
     </div>
   </div>
 );
 
-const CourseList = ({ courses }) => (
-  <div className="course-list">
-    { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
+const TermButton = ({term, setTerm, checked}) => (
+  <>
+    <input type="radio" id={term} className="btn-check" checked={checked} autoComplete="off"
+      onChange={() => setTerm(term)} />
+    <label class="btn btn-success m-1 p-2" htmlFor={term}>
+    { term }
+    </label>
+  </>
+);
+
+const TermSelector = ({term, setTerm}) => (
+  <div className="btn-group">
+  { 
+    Object.values(terms).map(value => (
+      <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
+    ))
+  }
   </div>
 );
+
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = useState('Spring');
+  const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+  
+  return (
+    <>
+      <TermSelector term={term} setTerm={setTerm} />
+      <div className="course-list">
+      { termCourses.map(course => <Course key={course.id} course={ course } />) }
+      </div>
+   </>
+  );
+};
 
 const Banner = ({ title }) => (
   <h1>{ title }</h1>
 );
 
-const App = () =>  (
-  <div className="container">
-    <Banner title={ schedule.title } />
-    <CourseList courses={ schedule.courses } />
-  </div>
-);
+const App = () => {
+  const [schedule, setSchedule] = useState();
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, []);
+
+  if (!schedule) return <h1>Loading schedule...</h1>;
+
+  return (
+    <div className="container">
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
+    </div>
+  );
+};
 
 export default App;
