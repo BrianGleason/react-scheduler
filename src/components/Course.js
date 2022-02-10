8cf36708dbd74dbd26fb408488a9ccfaf3661c6d
Brian Gleason <51419601+BrianGleason@users.noreply.github.com>
2022-02-09 21:36:11 -0600

    can store data (change time conflicts)

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,9 +1,28 @@
-import { hasConflict, getCourseTerm } from '../utilities/time.js';
+import { hasConflict, getCourseTerm, timeParts } from '../utilities/time.js';
+import { setData } from '../utilities/firebase.js';
 
+  
+const getCourseMeetingData = course => {
+    const meets = prompt('Enter meeting data: MTuWThF hh:mm-hh:mm', course.meets);
+    const valid = !meets || timeParts(meets).days;
+    if (valid) return meets;
+    alert('Invalid meeting data');
+    return null;
+  };
 
-const getCourseNumber = course => (
-    course.id.slice(1, 6)
+  const getCourseNumber = course => (
+    course.id.slice(1, 4)
   );
+
+  const reschedule = async (course, meets) => {
+    if (meets && window.confirm(`Change ${course.id} to ${meets}?`)) {
+      try {
+        await setData(`/courses/${course.id}/meets`, meets);
+      } catch (error) {
+        alert(error);
+      }
+    }
+  };
   
 export const Course = ({ course, selected, setSelected }) => {
     const isSelected = selected.includes(course);
@@ -11,19 +30,21 @@ export const Course = ({ course, selected, setSelected }) => {
     const style = {
       backgroundColor: isDisabled? 'lightgrey' : isSelected ? 'lightgreen' : 'white'
     };
+  
     return (
       <div className="card m-1 p-2" 
-        style={style}
-        onClick={isDisabled ? null : () =>  setSelected(toggle(course, selected))}>
+          style={style}
+          onClick={isDisabled ? null : () =>  setSelected(toggle(course, selected))}
+          onDoubleClick={() => reschedule(course, getCourseMeetingData(course))}>
         <div className="card-body">
           <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
           <div className="card-text">{ course.title }</div>
+          <div className="card-text">{ course.meets }</div>
         </div>
       </div>
     );
   };
-  
-  
+
   const toggle = (x, lst) => (
     lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
   );
